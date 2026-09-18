7e321a7054d263863be925b9f710968e78e6fba1
Daniel Leaberry <leaberry@gmail.com>
2026-09-18 09:20:37 -0600

    Bump firmware and web asset version to 1.4.15

--- AAA_INCLUDES.h
+++ AAA_INCLUDES.h
@@ -41,7 +41,7 @@
 #include <esp_system.h>
 //#include <Hash.h>
 #include "PSACrypto.h"
-#define VERSION  "ESP32C6-ECU_v1_4_14"
+#define VERSION  "ESP32C6-ECU_v1_4_15"
 
 #include <TimeLib.h>
 #include <time.h>
